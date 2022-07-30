8d1b122b82b457a9dcec5921f1982a23c4f5df4b
Ilya Merkulov <imerkulov@me.com>
2022-07-31 00:41:02 +0300

    Add react-redux basic legacy example

---
+++ src/legacyStore/BasicLegacyReactApp.tsx
@@ -0,0 +1,27 @@
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { store } from './index';
+
+function App() {
+  const dispatch = useDispatch();
+  const value = useSelector<{ value: number }, number>((state) => state.value);
+
+  //  Number(prompt('How many?'))
+  return (
+    <div>
+      <h2>Basic Legacy React App</h2>
+      <button onClick={() => dispatch({ type: 'counter/increment', payload: 2 })}>+</button>
+      <button onClick={() => dispatch({ type: 'counter/decrement' })}>-</button>
+      {value}
+    </div>
+  );
+}
+
+function BasicLegacyReactApp() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+
+export default BasicLegacyReactApp;
